a23ae23abe23fc33e41f42e30b78225d63090406
SindreMohr <sindre.mohr@bouvet.no>
2022-09-30 13:31:08 +0200

    added multiplication

--- example.py
+++ example.py
@@ -11,3 +11,10 @@ def subtract(a, b):
 def test_subtract():
 	assert subtract(2,3) == -1
 	assert subtract(3,3) == 0
+
+def multiply(a,b):
+	return a * b
+
+def test_multiply():
+	assert multiply(5,5) == 25
+
